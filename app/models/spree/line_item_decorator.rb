2b9c7832401c07126953b0beb6451cfa592fa64c
Alex Costantini <therealcos@gmail.com>
2018-01-05 19:12:17 -0500

    wip

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,6 +1,6 @@
 Spree::LineItem.class_eval do
   def tax_cloud_cache_key
-    key = "Spree::LineItem #{id}: #{quantity}x<#{variant.cache_key}>@#{price}#{currency}promo_total<#{promo_total}>"
+    key = "Spree::LineItem #{id}: #{quantity}x<#{variant.cache_key}>@#{price}#{currency}promo_amount<#{promo_total}>"
     if order.ship_address
       key << "shipped_to<#{order.ship_address.try(:cache_key)}>"
     elsif order.bill_address
@@ -9,14 +9,10 @@ Spree::LineItem.class_eval do
   end
 
   def promo_amount
-    promo = order.promotion
+    promo = order.adjustments.competing_promos.eligible.reorder("amount ASC, created_at DESC, id DESC").first
     return promo_total unless promo
     order_total = order.item_total
     return 0.0 unless order_total != 0.0
     (promo.amount * amount) / order_total
-  end
-
-  def discounted_amount
-    amount + promo_amount
   end
 end
