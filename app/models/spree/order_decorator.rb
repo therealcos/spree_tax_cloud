469f325c7b7fc46c734cd4f5f5026ef4a30ee22c
Alex Costantini <therealcos@gmail.com>
2018-02-12 22:47:01 -0500

    wip

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,6 +5,7 @@ Spree::Order.class_eval do
     return unless is_taxed_using_tax_cloud?
 
     self.shipments.each do |shipment|
+      next unless shipment.stock_location.country.iso == 'US'
       response = Spree::TaxCloud.transaction_from_item(shipment).authorized_with_capture
       if response != "OK"
         Rails.logger.error "ERROR: TaxCloud returned an order capture response of #{response}."
