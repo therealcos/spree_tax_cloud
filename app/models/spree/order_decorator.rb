c8cb98670d68613c7249a0dd8dacda590bb2c093
Alex Costantini <therealcos@gmail.com>
2018-05-12 20:29:30 -0400

    wip

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,27 +1,27 @@
-Spree::Order.class_eval do
-  self.state_machine.after_transition to: :complete, do: :capture_tax_cloud
+# Spree::Order.class_eval do
+#   self.state_machine.after_transition to: :complete, do: :capture_tax_cloud
 
-  def capture_tax_cloud
-    return unless is_taxed_using_tax_cloud?
-    response =  Spree::TaxCloud.transaction_from_order(self).authorized_with_capture
-    if response != "OK"
-      Rails.logger.error "ERROR: TaxCloud returned an order capture response of #{response}."
-    end
-    log_tax_cloud(response)
-  end
+#   def capture_tax_cloud
+#     return unless is_taxed_using_tax_cloud?
+#     response =  Spree::TaxCloud.transaction_from_order(self).authorized_with_capture
+#     if response != "OK"
+#       Rails.logger.error "ERROR: TaxCloud returned an order capture response of #{response}."
+#     end
+#     log_tax_cloud(response)
+#   end
 
-  # TaxRate.match is used here to check if the order is taxable by Tax Cloud.
-  # It's not possible check against the order's tax adjustments because
-  # an adjustment is not created for 0% rates. However, US orders must be
-  # submitted to Tax Cloud even when the rate is 0%.
-  def is_taxed_using_tax_cloud?
-    Spree::TaxRate.match(self.tax_zone).any? { |rate| rate.calculator_type == "Spree::Calculator::TaxCloudCalculator" }
-  end
+#   # TaxRate.match is used here to check if the order is taxable by Tax Cloud.
+#   # It's not possible check against the order's tax adjustments because
+#   # an adjustment is not created for 0% rates. However, US orders must be
+#   # submitted to Tax Cloud even when the rate is 0%.
+#   def is_taxed_using_tax_cloud?
+#     Spree::TaxRate.match(self.tax_zone).any? { |rate| rate.calculator_type == "Spree::Calculator::TaxCloudCalculator" }
+#   end
 
-  def log_tax_cloud(response)
-    # Implement into your own application.
-    # You could create your own Log::TaxCloud model then use either HStore or
-    # JSONB to store the response.
-    # The response argument carries the response from an order transaction.
-  end
-end
+#   def log_tax_cloud(response)
+#     # Implement into your own application.
+#     # You could create your own Log::TaxCloud model then use either HStore or
+#     # JSONB to store the response.
+#     # The response argument carries the response from an order transaction.
+#   end
+# end
