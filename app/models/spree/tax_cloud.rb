04c44d88a7b801cbaafb2c3a99bddb698368f47a
Alex Costantini <therealcos@gmail.com>
2018-11-07 15:24:39 -0500

    wip

--- app/models/spree/tax_cloud.rb
+++ app/models/spree/tax_cloud.rb
@@ -46,34 +46,25 @@ module Spree
     def self.cart_item_from_item(item, index)
       call_id = Array.new(36){[*"a".."z", *"A".."Z", *"0".."9"].sample}.join
 
-      puts "call_id is..."
-      puts call_id
-
       case item
       when Spree::LineItem
         order_promo = item.order.adjustments.competing_promos.eligible.reorder("amount ASC, created_at DESC, id DESC").first
         order_promo_amount = order_promo ? (order_promo.amount / item.order.quantity) : 0
         ::TaxCloud::CartItem.new(
           index:    index,
-          item_id:  "LineItem #{item.id}",
+          item_id:  "LineItem #{item.id} - #{call_id}",
           tic:      (item.product.tax_cloud_tic || Spree::Config.taxcloud_default_product_tic),
           price:    item.quantity == 0 ? item.price : (item.promo_total / item.quantity) + order_promo_amount + item.price,
           quantity: item.quantity
         )
-
-        puts "line item id is..."
-        puts "LineItem #{item.id} - #{call_id}"
       when Spree::Shipment
         ::TaxCloud::CartItem.new(
           index:    index,
-          item_id:  "Shipment #{item.number}",
+          item_id:  "Shipment #{item.number} - #{call_id}",
           tic:      Spree::Config.taxcloud_shipping_tic,
           price:    item.cost,
           quantity: 1
         )
-
-        puts "shipment id is..."
-        puts "Shipment #{item.number} - #{call_id}"
       else
         raise Spree.t(:cart_item_cannot_be_made)
       end
